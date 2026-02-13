381b0cb49c776b5845ea96f61bc70e1750256f7c
umar <umarsaleem512452@gmail.com>
2026-02-13 12:02:28 +0500

    starter template ready

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,5 +3,19 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  sdsd
+  <div class="main-wrapper">
+    <div class="text-black" style="color: black;">eheh</div>
+  </div>
 </template>
+
+<style>
+.layout-main-section-wrapper{
+  position: absolute !important;
+  top: 0 !important;
+  left: 0 !important;
+  width: 100% !important;
+  height: 100% !important;
+  background-color: blue;
+  z-index: 10000 !important;
+}
+</style>
