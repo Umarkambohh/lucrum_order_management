7eccfe217a106ed8e25e023ea838d09f98203a6c
umar <umarsaleem512452@gmail.com>
2026-02-13 12:57:32 +0500

    fix page appearness

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,39 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div class="main-wrapper">
-    <div class="text-black" style="color: black;">eheh</div>
+    <div class="text-black" style="color: black;">
+      <div v-for="i in 100">Ready to use</div>
+    </div>
   </div>
 </template>
 
+<script setup>
+</script>
+
 <style>
-.layout-main-section-wrapper{
-  position: absolute !important;
-  top: 0 !important;
-  left: 0 !important;
-  width: 100% !important;
-  height: 100% !important;
-  background-color: blue;
+#app {
+  position: fixed;
+  background: white;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   z-index: 10000 !important;
+  overflow-y: scroll;
 }
+
+/* Hide scrollbar for webkit browsers */
+#app::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for Firefox */
+#app {
+  scrollbar-width: none; /* Firefox */
+}
+
+/* Optional: Hide scrollbar when user is hovering over the area (better UX in some cases) */
+#app:hover::-webkit-scrollbar {
+  display: none;
+}
+
 </style>
